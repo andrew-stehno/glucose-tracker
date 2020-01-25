563be62fb1735a843c78b86ff037b8da07bd2bed
andrew stehno <andrew_stehno@yahoo.com>
2020-01-25 14:00:52 -0700

    moved react-router and react-router-dom from BE to FE pck.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // import React, { Component } from "react";
-// // import { Button } from 'reactstrap';
+// import { Button } from 'reactstrap';
 // // see npm reactstrap!!!
 
 // class App extends Component {
