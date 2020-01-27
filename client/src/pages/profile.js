efef52d154f52ec14afd45a7c9ae7fbd52ef340d
andrew stehno <andrew_stehno@yahoo.com>
2020-01-26 20:13:31 -0700

    roughed out input form wit date picker.

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,18 +1,17 @@
 import ProfileCard from "../components/ProfileCard/ProfileCard";
-import Form from "../components/Form/Form";
+import FormPage from "../components/InputForm/InputForm";
 import React from "react";
 import { Row, Container, Col } from "reactstrap";
 
 const Profile = () => {
   return (
     <Container>
-      <h1>hello</h1>
       <Row>
         <Col md="2">
           <ProfileCard />
         </Col>
         <Col md="10">
-          <Form />
+          <FormPage />
         </Col>
       </Row>
     </Container>
