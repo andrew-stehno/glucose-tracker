8c5d0c877f795e350fbf7c5a1e99784147f2b05f
lpettit1 <50005432+lpettit1@users.noreply.github.com>
2020-01-29 16:10:47 -0700

    init change

---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Jumbotron from "../../components/Jumbotron/Jumbotron";
+import Footer from "../../components/Footer/Footer";
+import { Container } from "reactstrap";
+
+
+
+
+const Home = () => {
+    return (
+        <Jumbotron />,
+        <Footer />
+        
+
+};
+
+ 
+
+export default Home;
+
