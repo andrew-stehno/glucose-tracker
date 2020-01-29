637f3e5be743ff7ef69840f6271d1cabfc515eb6
Matt Wigdahl <matt@leverpulley.com>
2020-01-28 17:49:22 -0700

    added some data to chart

--- client/src/components/DiabetesChart/DiabetesChart.js
+++ client/src/components/DiabetesChart/DiabetesChart.js
@@ -8,30 +8,33 @@ import {
 import "@devexpress/dx-react-chart-bootstrap4/dist/dx-react-chart-bootstrap4.css";
 
 
-
 const generateData = (start, end, step) => {
   const data = [{
-    value: 1.12312,
-    argument: 1
+    "value": 150.12312,
+    "argument": 1
   },
   {
-    value: 1.432232,
-    argument: 2
+    "value": 175.432232,
+    "argument": 2
   },
   {
-    value: 2.45433,
-    argument: 3
+    "value": 200.45433,
+    "argument": 3
   },{
-    value: 1.12312,
-    argument: 4
+    "value": 199.12312,
+    "argument": 4
   },
   {
-    value: 1.432232,
-    argument: 5
+    "value": 125.432232,
+    "argument": 5
   },
   {
-    value: 2.45433,
-    argument: 6
+    "value": 123.45433,
+    "argument": 6
+  },
+  {
+    "value": 103.45433,
+    "argument": 6
   }];
   // for (let i = start; i < end; i += step) {
   //   data.push({ value: Math.sin(i) / i, argument: i });
@@ -47,7 +50,7 @@ export default class DiabetesChart extends React.PureComponent {
     super(props);
 
     this.state = {
-      data: generateData(2, 12, 0.25)
+      data: generateData(150, 200, 1)
     };
   }
   render() {
