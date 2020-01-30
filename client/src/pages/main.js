8351647385ecfe2f01c3afafd68233cefa37f6d6
andrew stehno <andrew_stehno@yahoo.com>
2020-01-30 16:57:56 -0700

    dynamically populate chart function with data

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -14,13 +14,14 @@ import {
   Label,
   Input
 } from "reactstrap";
-import AlertHelper from "../components/AlertHelper/AlertHelper";
+// import AlertHelper from "../components/AlertHelper/AlertHelper";
 
 class Main extends React.Component {
   state = {
     startDate: new Date(),
     glucoseLevel: "",
-    results: []
+    results: [],
+    chartData: []
   };
 
   handleChange = date => {
@@ -37,23 +38,85 @@ class Main extends React.Component {
   };
 
   saveToDatabase = () => {
+    // Post data to database:
     API.saveData({
       date: this.state.startDate,
       glucose: this.state.glucoseLevel
     })
       .then(res => {
         let date = this.state.startDate;
-        console.log("Date: " + date);
+        // Get data from db by specific day:
         API.getByDay(date).then(res => {
           this.setState({
             results: res.data,
             startDate: new Date(),
             glucoseLevel: ""
           });
-          console.log(this.state.results);
-        });
+          // Massage raw data into useable data:
+          const resData = this.state.results;
+          const newArray = [];
+          for (let i = 0; i < resData.length; i++) {
+            const item = resData[i];
+            let newObj = {
+              "value": item.glucose,
+              "high": 130,
+              "low": 80,
+              "date": [i],
+              "realDate": item.date
+            }
+            newArray.unshift(newObj);
+          }
+          this.setState({chartData: newArray});
+          console.log(this.state.chartData);
+        })
       })
       .catch(err => console.log(err));
+  };
+
+  generateData = (start, end, step) => {
+    const data = this.state.chartData;
+    // const data = [{
+    //   "value": 85.12312,
+    //   "high" : 130,
+    //   "low" : 80,
+    //   "date": 10
+    // },
+    // {
+    //   "value": 100.43,
+    //   "high" : 130,
+    //   "low" : 80,
+    //   "date": 25
+    // },
+    // {
+    //   "value": 150.45433,
+    //   "high" : 130,
+    //   "low" : 80,
+    //   "date": 30
+    // },{
+    //   "value": 40.12312,
+    //   "high" : 130,
+    //   "low" : 80,
+    //   "date": 45
+    // },
+    // {
+    //   "value": 110.432232,
+    //   "date": 50.1
+    // },
+    // {
+    //   "value": 100.45433,
+    //   "high" : 130,
+    //   "low" : 80,
+    //   "date": 60
+    // },
+    // {
+    //   "value": 80.45433,
+    //   "high" : 130,
+    //   "low" : 80,
+    //   "date": 70
+    // }];
+    console.log("data", data);
+    
+    return data;
   };
 
   render() {
@@ -88,8 +151,11 @@ class Main extends React.Component {
                 <Button onClick={() => this.saveToDatabase()}>Submit</Button>
               </Form>
               <br />
-              <DiabetesChart />
-              <AlertHelper />
+              <DiabetesChart 
+                results={this.state.results}
+                generateData={this.generateData}
+              />
+              {/* <AlertHelper /> */}
             </Col>
           </Row>
         </Container>
