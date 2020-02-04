9b8a54fa0ae675dcd2e7fb9a0f5edb501e309047
andrew stehno <andrew_stehno@yahoo.com>
2020-02-04 15:41:20 -0700

    build

---
+++ client/build/precache-manifest.20b1e506792e2aa0125a3d14e694381e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "873bcaa45c04456100b4",
+    "url": "/static/js/main.e91d4484.chunk.js"
+  },
+  {
+    "revision": "c8e66c186e54c958d673",
+    "url": "/static/js/2.b510e729.chunk.js"
+  },
+  {
+    "revision": "873bcaa45c04456100b4",
+    "url": "/static/css/main.854a6100.chunk.css"
+  },
+  {
+    "revision": "c8e66c186e54c958d673",
+    "url": "/static/css/2.f4abd768.chunk.css"
+  },
+  {
+    "revision": "2029cad1a821faa2c80a88120e329a3d",
+    "url": "/index.html"
+  }
+];
