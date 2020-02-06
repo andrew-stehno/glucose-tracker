86c318dce0f848947311624829a411e4fbff88c7
andrew stehno <andrew_stehno@yahoo.com>
2020-02-05 18:15:01 -0700

    npm run build

--- client/build/precache-manifest.8a4de5594dff71fd24e71d2dea169a17.js
+++ client/build/precache-manifest.8a4de5594dff71fd24e71d2dea169a17.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "143eb6c079413d5e9967",
-    "url": "/static/js/main.e5839c3a.chunk.js"
+    "revision": "a07da3f7931e34c121f9",
+    "url": "/static/js/main.02e3c289.chunk.js"
   },
   {
     "revision": "31ae4f9eca7485b23765",
     "url": "/static/js/2.8fd63cbb.chunk.js"
   },
   {
-    "revision": "143eb6c079413d5e9967",
+    "revision": "a07da3f7931e34c121f9",
     "url": "/static/css/main.081d8515.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/2.c0eba2c5.chunk.css"
   },
   {
-    "revision": "5db114ffc8dc98c55dcb36fbae54093c",
+    "revision": "2ce52c8a8d1135089bf15297552d7c36",
     "url": "/index.html"
   }
 ];
