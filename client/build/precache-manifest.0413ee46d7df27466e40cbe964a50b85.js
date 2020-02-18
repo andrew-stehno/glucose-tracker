029b40c233c26684d66aac38273a165b272629eb
andrew stehno <andrew_stehno@yahoo.com>
2020-02-18 13:37:16 -0700

    updated chart high value

---
+++ client/build/precache-manifest.0413ee46d7df27466e40cbe964a50b85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d47045ad79dc8bd26aee",
+    "url": "/static/js/main.b10ec59d.chunk.js"
+  },
+  {
+    "revision": "6491194b97c65c79ac23",
+    "url": "/static/js/2.4b0c4f15.chunk.js"
+  },
+  {
+    "revision": "d47045ad79dc8bd26aee",
+    "url": "/static/css/main.f8cf2157.chunk.css"
+  },
+  {
+    "revision": "6491194b97c65c79ac23",
+    "url": "/static/css/2.ae2ad103.chunk.css"
+  },
+  {
+    "revision": "ab556259581800b410ab66a67851ffb6",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f8f2ee3e8727a0028fa5703cfdbe7d8.js"
+  "/precache-manifest.0413ee46d7df27466e40cbe964a50b85.js"
 );
 
 workbox.clientsClaim();
